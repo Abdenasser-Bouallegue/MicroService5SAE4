b196dd00cb34e9a13691d9e16e162a0c82e98113
Mansour <mansour.frikha@esprit.tn>
2024-11-10 22:11:38 +0100

    test front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CoursesComponent } from './courses/courses.component';
+import { CourseListComponent } from './course-list/course-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'courses', component: CoursesComponent }, // Liste des cours
+  { path: '', redirectTo: '/courses', pathMatch: 'full' }, 
+  { path: 'coursesss', component: CourseListComponent },// Redirection par défaut
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CourseListComponent } from './course-list/course-list.component';  // Importez le module de routage ici
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    CourseListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule  // Ajoutez le module de routage ici
   ],
   providers: [],
   bootstrap: [AppComponent]
